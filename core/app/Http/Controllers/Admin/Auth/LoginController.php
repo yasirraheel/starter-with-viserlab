f225d92d7dbcb480ae512ad1530f8a4e4ca523f6
Muhammad Asif Shabbir <74806002+yasirraheel@users.noreply.github.com>
2026-06-19 07:45:35 +0500

    Bypass ViserLab purchase code validation

--- core/app/Http/Controllers/Admin/Auth/LoginController.php
+++ core/app/Http/Controllers/Admin/Auth/LoginController.php
@@ -67,7 +67,7 @@ class LoginController extends Controller
         }
 
 
-        Onumoti::getData();
+        // Onumoti::getData();
 
         // If the class is using the ThrottlesLogins trait, we can automatically throttle
         // the login attempts for this application. We'll key this by the username and
--- core/app/Http/Controllers/Controller.php
+++ core/app/Http/Controllers/Controller.php
@@ -9,7 +9,7 @@ abstract class Controller
     public function __construct()
     {
         $className = get_called_class();
-        Onumoti::mySite($this,$className);
+        // Onumoti::mySite($this,$className);
     }
 
     public static function middleware()
